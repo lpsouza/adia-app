64c83397c340839c90b81163d8b38fdc300289e2
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2022-07-31 02:20:14 -0300

    Change 404 error page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,111 +1,27 @@
 import {
   Box,
   Card,
-  Typography,
-  Container,
-  Divider,
-  Button,
-  FormControl,
-  OutlinedInput,
-  InputAdornment,
-  styled
+  CardContent,
+  CardHeader,
+  Divider
 } from '@mui/material';
-import Head from 'next/head';
-import SearchTwoToneIcon from '@mui/icons-material/SearchTwoTone';
-import type { ReactElement } from 'react';
-import BaseLayout from 'src/layouts/BaseLayout';
 
-const MainContent = styled(Box)(
-  () => `
-    height: 100%;
-    display: flex;
-    flex: 1;
-    flex-direction: column;
-`
-);
-
-const TopWrapper = styled(Box)(
-  ({ theme }) => `
-  display: flex;
-  width: 100%;
-  flex: 1;
-  align-items: center;
-  justify-content: center;
-  padding: ${theme.spacing(6)};
-`
-);
-
-const OutlinedInputWrapper = styled(OutlinedInput)(
-  ({ theme }) => `
-    background-color: ${theme.colors.alpha.white[100]};
-`
-);
-
-const ButtonSearch = styled(Button)(
-  ({ theme }) => `
-    margin-right: -${theme.spacing(1)};
-`
-);
-
-function Status404() {
+const Error404Page = () => {
   return (
-    <>
-      <Head>
-        <title>Status - 404</title>
-      </Head>
-      <MainContent>
-        <TopWrapper>
-          <Container maxWidth="md">
-            <Box textAlign="center">
-              <img alt="404" height={180} src="/static/images/status/404.svg" />
-              <Typography variant="h2" sx={{ my: 2 }}>
-                The page you were looking for doesn't exist.
-              </Typography>
-              <Typography
-                variant="h4"
-                color="text.secondary"
-                fontWeight="normal"
-                sx={{ mb: 4 }}
-              >
-                It's on us, we moved the content to a different page. The search
-                below should help!
-              </Typography>
-            </Box>
-            <Container maxWidth="sm">
-              <Card sx={{ textAlign: 'center', mt: 3, p: 4 }}>
-                <FormControl variant="outlined" fullWidth>
-                  <OutlinedInputWrapper
-                    type="text"
-                    placeholder="Search terms here..."
-                    endAdornment={
-                      <InputAdornment position="end">
-                        <ButtonSearch variant="contained" size="small">
-                          Search
-                        </ButtonSearch>
-                      </InputAdornment>
-                    }
-                    startAdornment={
-                      <InputAdornment position="start">
-                        <SearchTwoToneIcon />
-                      </InputAdornment>
-                    }
-                  />
-                </FormControl>
-                <Divider sx={{ my: 4 }}>OR</Divider>
-                <Button href="/" variant="outlined">
-                  Go to homepage
-                </Button>
-              </Card>
-            </Container>
-          </Container>
-        </TopWrapper>
-      </MainContent>
-    </>
+    <Box sx={{
+      width: 600,
+      height: 100,
+      margin: "auto",
+    }}>
+      <Card>
+        <CardHeader title="Error 404" />
+        <Divider />
+        <CardContent>
+          Page not found
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
 
-export default Status404;
-
-Status404.getLayout = function getLayout(page: ReactElement) {
-  return <BaseLayout>{page}</BaseLayout>;
-};
+export default Error404Page;
