46eebe919f93406d670eb76196e917e53928e46b
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2022-07-31 02:20:54 -0300

    Transform index to initial loader

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,89 +1,37 @@
+import { useEffect } from 'react';
 import {
-  Typography,
   Box,
-  Card,
-  Container,
-  Button,
-  styled
+  CircularProgress
 } from '@mui/material';
-import type { ReactElement } from 'react';
-import BaseLayout from 'src/layouts/BaseLayout';
 
-import Link from 'src/components/Link';
-import Head from 'next/head';
+import Router from 'next/router';
 
-import Logo from 'src/components/LogoSign';
-import Hero from 'src/content/Overview/Hero';
+const LoadingPage = () => {
+  useEffect(() => {
+    const accessToken = localStorage.getItem('access_token');
+    fetch('http://localhost:3001/auth/token', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    }).then(res => {
+      if (res.status === 200) {
+        Router.push('/app');
+      } else {
+        Router.push('/login');
+      }
+    });
+  });
 
-const HeaderWrapper = styled(Card)(
-  ({ theme }) => `
-  width: 100%;
-  display: flex;
-  align-items: center;
-  height: ${theme.spacing(10)};
-  margin-bottom: ${theme.spacing(10)};
-`
-);
-
-const OverviewWrapper = styled(Box)(
-  ({ theme }) => `
-    overflow: auto;
-    background: ${theme.palette.common.white};
-    flex: 1;
-    overflow-x: hidden;
-`
-);
-
-function Overview() {
   return (
-    <OverviewWrapper>
-      <Head>
-        <title>Tokyo Free Black NextJS Typescript Admin Dashboard</title>
-      </Head>
-      <HeaderWrapper>
-        <Container maxWidth="lg">
-          <Box display="flex" alignItems="center">
-            <Logo />
-            <Box
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              flex={1}
-            >
-              <Box />
-              <Box>
-                <Button
-                  component={Link}
-                  href="/dashboards/tasks"
-                  variant="contained"
-                  sx={{ ml: 2 }}
-                >
-                  Live Preview
-                </Button>
-              </Box>
-            </Box>
-          </Box>
-        </Container>
-      </HeaderWrapper>
-      <Hero />
-      <Container maxWidth="lg" sx={{ mt: 8 }}>
-        <Typography textAlign="center" variant="subtitle1">
-          Crafted by{' '}
-          <Link
-            href="https://bloomui.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            BloomUI.com
-          </Link>
-        </Typography>
-      </Container>
-    </OverviewWrapper>
+    <Box sx={{
+      width: 100,
+      height: 100,
+      margin: "auto",
+    }}>
+      <CircularProgress />
+    </Box>
   );
 }
 
-export default Overview;
-
-Overview.getLayout = function getLayout(page: ReactElement) {
-  return <BaseLayout>{page}</BaseLayout>;
-};
+export default LoadingPage;
