5509dc0f8540ade22bfc2780d991670b84d8bd8c
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2022-08-15 16:32:22 -0300

    Changes on old SessionLogin to Loader function

--- components/SessionLogin/index.tsx
+++ components/SessionLogin/index.tsx
@@ -1,8 +1,41 @@
 import CoreService from "@/services/CoreService";
+import themeSelector from "@/styles/themeSelector";
+import { Box, createTheme, keyframes, styled } from "@mui/material";
 import Router from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-const SessionLogin = () => {
+const theme = createTheme(themeSelector());
+
+const SpinAnimation = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+const LoadingScreen = styled(Box)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: ${theme.palette.background.default};
+  z-index: 100;
+`;
+
+const Loading = styled(Box)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: 5px solid ${theme.palette.grey[800]};
+  border-top: 5px solid ${theme.palette.primary.main};
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: ${SpinAnimation} 1.5s linear infinite;
+`;
+
+const Loader = () => {
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
@@ -19,17 +52,21 @@ const SessionLogin = () => {
           const token = await res.json();
           window.localStorage.setItem('access_token', token.access);
           window.localStorage.setItem('refresh_token', token.refresh);
-          Router.push('/');
+          await Router.push('/');
         }
         const excludePages = ['/auth/login', '/auth/register', '/404']
         if (!excludePages.includes(Router.pathname)) {
-          Router.push('/auth/login');
+          await Router.push('/auth/login');
         }
       }
-
+      setLoading(false);
     })();
   }, [])
-  return (<></>);
+  return (
+    <>
+      {loading && <LoadingScreen><Loading /></LoadingScreen>}
+    </>
+  );
 }
 
-export default SessionLogin;
+export default Loader;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,8 @@ import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 import { EmotionCache } from '@emotion/cache';
 import { CacheProvider } from '@emotion/react';
 import createEmotionCache from '@/styles/createEmotionCache';
-import darkThemeOptions from '@/styles/theme/dark';
-import SessionLogin from '@/components/SessionLogin';
+import Loader from '@/components/SessionLogin';
+import themeSelector from '@/styles/themeSelector';
 
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
@@ -20,7 +20,7 @@ const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
     <CacheProvider value={emotionCache}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <SessionLogin />
+        <Loader />
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
