a3d6f389a554e066495653e979c82f58fcd3c3f8
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2022-08-15 16:36:08 -0300

    Changes on refresh token logic

--- components/SessionLogin/index.tsx
+++ components/SessionLogin/index.tsx
@@ -41,22 +41,22 @@ const Loader = () => {
     (async () => {
       const accessToken = window.localStorage.getItem('access_token');
       const refreshToken = window.localStorage.getItem('refresh_token');
-      const res = await CoreService.auth.token(accessToken || "");
-      if (res.status === 200) {
-        const data = await res.json();
+      const access = await CoreService.auth.token(accessToken || "");
+      if (access.status === 200) {
+        const data = await access.json();
         window.localStorage.setItem('email', data.email);
         window.localStorage.setItem('name', data.name);
       } else {
-        const res = await CoreService.auth.refresh(refreshToken || "");
-        if (res.status === 200) {
-          const token = await res.json();
+        const refresh = await CoreService.auth.refresh(refreshToken || "");
+        if (refresh.status === 200) {
+          const token = await refresh.json();
           window.localStorage.setItem('access_token', token.access);
           window.localStorage.setItem('refresh_token', token.refresh);
-          await Router.push('/');
-        }
-        const excludePages = ['/auth/login', '/auth/register', '/404']
-        if (!excludePages.includes(Router.pathname)) {
-          await Router.push('/auth/login');
+        } else {
+          const excludePages = ['/auth/login', '/auth/register', '/404']
+          if (!excludePages.includes(Router.pathname)) {
+            await Router.push('/auth/login');
+          }
         }
       }
       setLoading(false);
