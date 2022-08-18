754733c061eb161dc8b6e7a0cb4d24e4325152ed
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2022-08-18 00:30:54 -0300

    Remove unecessary element

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,12 @@ import SideMenuLayout from '@/components/SideMenuLayout'
 const Home: NextPage = () => {
   return (
     <SideMenuLayout>
-      <>
-        <Head>
-          <title>Inicial</title>
-        </Head>
-        <Box sx={{ padding: 3 }}>
-          <h1>Inicial</h1>
-        </Box>
-      </>
+      <Head>
+        <title>Inicial</title>
+      </Head>
+      <Box sx={{ padding: 3 }}>
+        <h1>Inicial</h1>
+      </Box>
     </SideMenuLayout>
   )
 }
