72607ac846cdde052abe465a49cb8aa196778476
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2022-11-11 17:43:32 -0300

    Changes on loader to hide TopMenu

--- components/Loader/index.tsx
+++ components/Loader/index.tsx
@@ -18,7 +18,7 @@ const LoadingScreen = styled(Box)`
   width: 100%;
   height: 100%;
   background-color: ${theme.palette.background.default};
-  z-index: 100;
+  z-index: 999999;
 `;
 
 const Loading = styled(Box)`
